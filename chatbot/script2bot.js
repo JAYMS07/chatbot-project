ab41ef916948091330ee0c2fd70bb568cf8f9aed
Jay sawwalakhe <jaysawwalakhe1234@gmail.com>
2025-03-22 11:29:31 +0530

    Update script2bot.js
    Secured API key using environment variables

--- chatbot/script2bot.js
+++ chatbot/script2bot.js
@@ -9,7 +9,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "AIzaSyAPrPKRaRYEjbq6AyeHy_Dt9t458uA_nHQ";
+const API_KEY = "your_api_key_here";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
